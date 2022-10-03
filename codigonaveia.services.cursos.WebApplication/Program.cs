150de9809430988eb96c391e758a789f683c0aee
= <Wagner@DESKTOP-8PTP712>
2022-10-03 10:51:07 -0300

    Adicionando melhorias e bugs na Api e UIAdd project files.

---
+++ codigonaveia.services.cursos.WebApplication/Program.cs
@@ -0,0 +1,37 @@
+using codigonaveia.services.cursos.WebApplication.Interfaces;
+using Refit;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//Para cada interface tipo alunos... etc criar outro addRefitclient 
+var clientHandler = new HttpClientHandler();
+builder.Services.AddRefitClient<ICursosRepository>()
+    .ConfigureHttpClient(c => {
+        c.BaseAddress = new Uri(builder.Configuration.GetValue<String>("Uri"));
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
